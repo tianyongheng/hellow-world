4b1732ad0d5523a4c264ec82912ca89ee96e632e
tian <tianyongheng_job@163.com>
2019-04-19 15:10:42 +0800

    123

--- src/coreJava01/ch1/FirstSample.java
+++ src/coreJava01/ch1/FirstSample.java
@@ -3,7 +3,7 @@ package coreJava01.ch1;
 public class FirstSample {
 
 	static {
-		System.out.println("1111");
+		System.out.println("11121");
 	}
 	public static void main(String[] args) {
 		System.out.println("We will not use 'Hello,World!'");
